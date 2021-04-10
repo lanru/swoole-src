a2ddb4648818111c9372c551259efd9ecc8856ae
17381792 <17381792@qq.com>
2021-04-10 21:52:38 +0800

    mac编译文件

--- 2.sh
+++ 2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
-export LDFLAGS="-L/usr/local/opt/libiconv/lib"
-export CPPFLAGS="-I/usr/local/opt/libiconv/include"
 ./configure --with-openssl-dir=/usr/local/opt/openssl@1.1 --enable-http2 --enable-debug --enable-trace-log
