c4b29e7371a6a337825b389dc06e9ea3a37b123a
17381792 <17381792@qq.com>
2021-04-11 08:53:23 +0800

    就是要你懂swoole-- 编程须知 https://zhuanlan.zhihu.com/p/40762899

---
+++ php_scripts/eval.php
@@ -0,0 +1,14 @@
+<?php
+
+$handle1 = fopen("file.txt", "r");
+$handle2 = fopen("file.txt", "r");
+
+$content = fread($handle1, 3);
+
+echo "The process reads three bytes through handle1, the content is: " . $content . PHP_EOL;
+
+$content1 = fread($handle1, 1);
+$content2 = fread($handle2, 1);
+
+echo "The process reads one bytes through handle1, the content is: " . $content1 . PHP_EOL;
+echo "The process reads one bytes through handle2, the content is: " . $content2 . PHP_EOL;
